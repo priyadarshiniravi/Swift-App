6d07fe52f87dddb309ff9c9f13867d1ed1792067
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2017-02-10 23:36:14 +0530

    Added appen with a button

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -1,25 +1,15 @@
-//
-//  ViewController.swift
-//  Sample
-//
-//  Created by Priyadarshani on 2/10/17.
-//  Copyright © 2017 priyadarshani. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var word1: UITextField!
+    
+    @IBOutlet weak var word2: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func onClick(sender: AnyObject) {
+        result.text = word1.text! + word2.text!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
