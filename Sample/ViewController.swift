a05411d785c524337a4e2e354e6f738e04c12326
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2017-02-13 11:53:06 +0530

    Added suspend keyboard when click around and click on login

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,7 +9,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     
     @IBAction func onClick(sender: AnyObject) {
+        self.word1.resignFirstResponder()
+        self.word2.resignFirstResponder()
         result.text = word1.text! + word2.text!
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
 
